c6e4950b506a0742c87c937b519d36e63f637a9a
Raicelys Suero <72804397+Raicelys21@users.noreply.github.com>
2021-12-06 17:22:00 -0400

    Add files via upload

---
+++ Parallel matrix/com/company/Main.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import secuencial.SequentialMatrix;
+import paralelo.ParallelMatrix;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+            
+        
+//        int a = 1;
+//        for(int i = 0; i < 10; i++) 
+//        {
+//        	 /* INVERSE*/
+//            //Secuencial
+//        	SequentialMatrix matrizS1 = new SequentialMatrix(a,a);
+//            double[][] InvSecuencial = matrizS1.inversa();
+//            
+//            /* INVERSE*/
+//            //Paralelo
+//        	ParallelMatrix matrizS2 = new ParallelMatrix(a,a);
+//            double[][] InvParalela = matrizS2.inversa();
+//            //Parallel
+//            a += 1;
+//            
+//            System.out.println("**********************************************");
+//            
+//        }
+        
+        /*ParallelMatrix matriz1 = new ParallelMatrix(5,5);
+        double[][] inversa1 = matriz1.inversa();
+        
+        matriz1.ImprimirMatriz(matriz1.matrixData);*/
+    	
+    	
+    
+        /*for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                double suma = 0;
+
+                for(int k = 0; k < 2; k++)
+                {
+                    suma += InvParalela[i][k] * matrizS2Data[k][j];
+                }
+                producto[i][j] = suma;
+
+            }
+
+        }
+        
+        matrizS2.ImprimirMatriz(producto);*/
+        
+        
+        /**
+         * Verificación de la matriz inversa
+         * */
+   
+
+
+
+    }
+    
+    
+    
+   
+}
+
+
+
+
